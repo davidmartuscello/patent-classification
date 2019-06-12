863e98927b5e28250c2dcca69b73bb847af072b0
David Martuscello <dm4350@nyu.edu>
2019-06-12 13:28:13 -0400

    add accuracy plots

--- Text-Classification-Pytorch-master/main.py
+++ Text-Classification-Pytorch-master/main.py
@@ -13,6 +13,7 @@ from models.LSTM_Attn import AttentionModel
 from models.RCNN import RCNN
 from models.RNN import RNN
 from bayes_opt import BayesianOptimization
+import matplotlib.pyplot as plt
 
 def clip_gradient(model, clip_value):
     params = list(filter(lambda p: p.grad is not None, model.parameters()))
@@ -121,11 +122,20 @@ def run_best_model(args):
     TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_patents.load_dataset(batch_size, cache_data=cache_data)
     weights = word_embeddings
     model = LSTMClassifier(batch_size, output_size, hidden_size, vocab_size, embedding_length, word_embeddings)
+
+    acc_list = []
+    val_acc = []
     for epoch in range(epochs):
-        print('EPOCH')
         #(model, train_iter, epoch, batch_size, learning_rate)
         train_loss, train_acc = train_model(model, train_iter, epoch, batch_size, learning_rate)
         val_loss, val_acc = eval_model(model, valid_iter, batch_size)
+        print(f'EPOCH {epoch} -- Train Loss: {train_loss:.3f}, Train Acc: {train_acc:.2f}%')
+        acc_list.append(train_acc)
+        val_list.append(val_acc)
+
+    plt.plot(acc_list, label="train")
+    plt.plot(val_list, label="val")
+    plt.savefig('acc_graph.png')
 
     test_loss, test_acc = eval_model(model, test_iter, batch_size)
     print("performance of model:")
